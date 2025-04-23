37a1bcb290cd97b90238daf51e4847f501b29e4e
Nikolay <106226408+NikolayAvramov@users.noreply.github.com>
2025-04-23 15:55:54 +0300

    debug login

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import app from "./app.js"; // Importing app using ES module syntax
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
+
 const port = process.env.PORT;
 
 const server = http.createServer(app);
